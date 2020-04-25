93f93b4b87a4c4c06f437a458910e68f55eb667a
komzy <komal.mariam@gmail.com>
2020-04-25 13:57:26 +0500

    added argument for gaussian matrix size GUI for heatmap generation, Merged levels heatmap

--- interface_heatmap.py
+++ interface_heatmap.py
@@ -7,6 +7,10 @@ from modules import heatmap_generation
 from openslide import open_slide
 from openslide.deepzoom import DeepZoomGenerator
 import json
+import re
+import csv
+import glob
+
 
 
 class DirectorySelection:
@@ -38,53 +42,58 @@ class HeatMapSettingsMenu:
 
     def __init__(self, master):
 
-        frame = tk.Frame(root)
-        frame.focus_force()
+        self.frame = tk.Frame(root)
+        self.frame.focus_force()
 
         with open(root.file_path+'/info.json') as f:
             data = json.load(f)
             file_path_default = data['File_Path']
             file_name = data['File_Name']
+            self.max_level = int(data['Level_Count']) - 1
 
         # File path of source .svs
-        label_file_name = tk.Label(frame, text="File Name: "+file_name)
+        label_file_name = tk.Label(self.frame, text="File Name: "+file_name)
         label_file_name.grid(column=1, row=0)
 
-        label_file_source_path = tk.Label(frame, text="File Source: ")
+        label_file_source_path = tk.Label(self.frame, text="File Source: ")
         label_file_source_path.grid(sticky="E", column=0, row=1)
 
         # Entry Box for File Path
-        entry_file_path = tk.Entry(frame, width=85)
-        entry_file_path.insert(0, file_path_default)
-        entry_file_path.grid(column=1, row=1)
+        self.entry_file_path = tk.Entry(self.frame, width=65)
+        self.entry_file_path.insert(0, file_path_default)
+        self.entry_file_path.grid(column=1, row=1)
 
         # Browse Button
-        browse = tk.Button(frame, text='Browse')
+        browse = tk.Button(self.frame, text='Browse')
         browse.grid(sticky="W", column=2, row=1, padx=(5,0))
 
         # Gaussian Matrix Size Selection Label
-        select_level = ttk.Label(frame, text="Gaussian Matrix Size: ")
-        select_level.grid(sticky="E", column=0, row=4)
-
-        def only_numeric_input(e):
-            # this is allowing all numeric input
-            if e.isdigit():
-                return True
-            # this will allow backspace to work
-            elif e == "":
-                return True
-            else:
-                return False
+        gaussian_matrix_size_label = ttk.Label(self.frame, text="Gaussian Matrix Size: ")
+        gaussian_matrix_size_label.grid(sticky="E", column=0, row=4)
 
 
         # Entry Box for Gaussian Matrix Selection
-        gaussian_matrix_size = tk.Entry(frame, width=10)
-        c = root.register(only_numeric_input)
-        gaussian_matrix_size.configure(validate="key", validatecommand=(c, '%P'))
-        gaussian_matrix_size.insert(0, 200)
-        gaussian_matrix_size.grid(sticky="W", column=1, row=4)
+        self.gaussian_matrix_size = tk.Entry(self.frame, width=10)
+        c = root.register(self.only_numeric_input)
+        self.gaussian_matrix_size.configure(validate="key", validatecommand=(c, '%P'))
+        self.gaussian_matrix_size.insert(0, 200)
+        self.gaussian_matrix_size.grid(sticky="W", column=1, row=4)
 
+        # Standard Deviation Label
+        sd_label = ttk.Label(self.frame, text="\t\t\tStandard Deviation:", state='disabled')
+        sd_label.grid(sticky="S",column=1, row=4)
 
+        # Entry Box for Standard Deviation Selection
+        self.standard_deviation = tk.Entry(self.frame, width=10, state='disabled')
+        self.standard_deviation.configure(validate="key", validatecommand=(c, '%P'))
+        self.standard_deviation.insert(0, 50)
+        self.standard_deviation.grid(sticky="E",column=1, row=4)
+
+        # Radio Button for level-wise & merged level
+        self.var = tk.IntVar()
+        tk.Radiobutton(self.frame, text="Level Wise", variable=self.var, value=1).grid(sticky="W",column=1, row=5)
+        tk.Radiobutton(self.frame, text="Merge All Levels \t \t   ", variable=self.var, value=2).grid(sticky="E",column=1, row=5)
+        self.var.set(1)
         # # Level Selection
         # select_level = ttk.Label(frame, text="Select Initial Level: ")
         # select_level.grid(sticky="E", column=0, row=4)
@@ -96,32 +105,89 @@ class HeatMapSettingsMenu:
         # selection.grid(sticky="W", column=1, row=4)
 
         # Confirm button
-        confirm = tk.Button(frame, text='OK')
-        confirm.grid(sticky="S", column=1, row=5, ipadx=10)
+        confirm = tk.Button(self.frame, text='OK')
+        confirm.grid(sticky="S", column=1, row=8, ipadx=10)
+
+       # self.merged_levels()
+        browse.bind('<ButtonRelease>', self.source_file_selection)
+        confirm.bind('<ButtonRelease>', self. on_button_press)
+        self.frame.pack(padx=50, pady=50)
+
+    def only_numeric_input(self,e):
+        # this is allowing all numeric input
+        if e.isdigit():
+            return True
+        # this will allow backspace to work
+        elif e == "":
+            return True
+        else:
+            return False
+
+    def source_file_selection(self,event):
+        new_file_path = filedialog.askopenfilename()
+        if not new_file_path == "":
+            self.entry_file_path.delete(0, "end")
+            self.entry_file_path.insert(0, new_file_path)
+
+    def merged_levels(self):
+        max_level_merged = []
+        count = 0;
+
+        #bring level points to maxlevel
+        os.chdir(root.file_path)
+        result = [i for i in glob.glob('Level *[0-9].csv')]
+        for file in result:
+            level = int(re.findall(r'\d+', file)[0])
+            scaling_factor = 2 ** (self.max_level - level)
+            with open(file) as csv_file:
+                reader = csv.reader(csv_file)
+                for row in reader:
+                    x = int(row[0]) * scaling_factor
+                    y = int(row[1]) * scaling_factor
+                    max_level_merged.append((x,y))
+                    count = count + 1
+
+
+        print "Count: "
+        print len(max_level_merged)
+
+        path = os.path.join(root.file_path, 'Merged_Levels')
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        for newCSV in result:
+            for level in range(7, self.max_level):
+                scaling_factor = 2 ** (self.max_level - level)
+                csv_output = open(os.path.join(path, "Level " + str(level) + ".csv"), "a")
+                with open(csv_output.name, 'wb+') as csv_file:
+                    writer = csv.writer(csv_file, delimiter=',')
+                    saved_points = []
+                    for x, y in max_level_merged:
+                        saved_points.append( ( int(round(x/scaling_factor)), int (round(y/scaling_factor) ) ) )
+
+                    writer.writerows(saved_points)
 
 
 
-        def source_file_selection(event):
-            new_file_path = filedialog.askopenfilename()
-            if not new_file_path == "":
-                entry_file_path.delete(0, "end")
-                entry_file_path.insert(0, new_file_path)
+    def on_button_press(self, event):
+        # Warning if source .svs not discovered
+        if not os.path.isfile(self.entry_file_path.get()):
+            messagebox.showerror("Error", "Cannot find file source")
+        else:
+            self.frame.pack_forget()
+            slide = open_slide(self.entry_file_path.get())
+            dz_generator = DeepZoomGenerator(slide)
 
-        def on_button_press(event):
-            # Warning if source .svs not discovered
-            if not os.path.isfile(entry_file_path.get()):
-                messagebox.showerror("Error", "Cannot find file source")
-            else:
-                frame.pack_forget()
-                slide = open_slide(entry_file_path.get())
-                dz_generator = DeepZoomGenerator(slide)
-
+            if self.var.get() == 1:
                 # Start main program
-                heatmap_generation.generate_heatmap(dz_generator, root.file_path, gaussian_matrix_size.get())
+                heatmap_generation.generate_heatmap(dz_generator, root.file_path, self.gaussian_matrix_size.get())
+            else:
+                self.merged_levels()
+                heatmap_generation.generate_heatmap(dz_generator,  os.path.join(root.file_path, 'Merged_Levels'),
+                                                    self.gaussian_matrix_size.get())
+                print "DONE"
 
-        browse.bind('<ButtonRelease>', source_file_selection)
-        confirm.bind('<ButtonRelease>', on_button_press)
-        frame.pack(padx=50, pady=50)
+
 
 
 def on_closing():
